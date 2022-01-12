9d21ed8f2755356bc36c92c28eaccf37a489cac4
cristiancfe <cristiancfe@gmail.com>
2022-01-11 23:04:10 -0300

    screenshot novos

--- features/step_definitions/google.rb
+++ features/step_definitions/google.rb
@@ -7,7 +7,7 @@ Quando('pesquisar pelo termo {string}') do |query|
   @google_home_page.search(query)
   @google_results_page = GoogleResultsPage.new   
 end                                                                                                                                               
-Entao('validar que foram retornados resultados') do                  
+Entao('validar que foram retornados resultados') do      
   expect(@google_results_page.current_url).to have_content 'q=Livelo'
   sleep(4)   
 end  
